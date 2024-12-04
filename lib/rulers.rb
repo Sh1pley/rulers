5a69368535cd842a9fd1589caf24009feabe7ad9
Jesse Shipley <jships@Jesses-MacBook-Pro.local>
2024-12-04 12:09:58 -0600

    the gem side

---
+++ lib/rulers.rb
@@ -0,0 +1,36 @@
+require "rulers/version"
+require "rulers/routing"
+require "rulers/util"
+require "rulers/dependencies"
+
+module Rulers
+  class Application
+    def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'content-type' => 'text/html'}, []]
+      elsif env['PATH_INFO'] == '/'
+        env['REQUEST_URI'] = '/quotes/a_quote' 
+        env['PATH_INFO'] = '/quotes/a_quote' 
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      begin
+        text = controller.send(act)
+        [200, {'content-type' => 'text.html'},
+        [text]]
+      rescue
+      end
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
+    end
+  end
+end
